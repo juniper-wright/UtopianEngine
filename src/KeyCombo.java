9a19dab96a8854d2e2c5142c16e0b6447278354e
phoenixfyre <ian.mcdevitt.4@gmail.com>
2013-10-12 10:40:26 -0400

    Updated the KeyCombo class.
    The KeyCombo class does very little currently. Eventually, I will do
    something along the lines of adding better logic for comparing input to
    keys instead of Line 22.

--- src/KeyCombo.java
+++ src/KeyCombo.java
@@ -3,14 +3,29 @@ class KeyCombo
 	private String _keyname;
 	private String _uscript;
 	
+	// Default constructor. Never used.
 	public KeyCombo()
 	{
-		
+		this._keyname = "DUMMY_KEY";
+		this._uscript = "";
 	}
 	
+	// 
 	public KeyCombo(String keyname, String uscript)
 	{
 		this._keyname = keyname;
 		this._uscript = uscript;
+	}
+	
+	public String getResults(String key)
+	{
+		if(this._keyname.equalsIgnoreCase(key))
+		{
+			return this._uscript;
+		}
+		else
+		{
+			return "";
+		}
 	}
 }
