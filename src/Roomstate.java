e944cbb64b1e4de26fbb177d82b86694e0f4915f
phoenixfyre <ian.mcdevitt.4@gmail.com>
2013-10-17 06:14:45 -0400

    Broke Room to pieces to create Roomstate class

---
+++ src/Roomstate.java
@@ -0,0 +1,23 @@
+public class Roomstate
+{
+	boolean _seen = false;
+	String _description;		// This is the long description for this roomstate
+	String _shortDescription;	// This is the short description for this roomstate
+	String[] _keyCombos;		// This is a list of key
+
+
+	public Roomstate()
+	{
+		
+	}
+	
+	public String description()
+	{
+		if (!this._seen)
+		{
+			this._seen = true;
+			return this._description;
+		}
+		return this._shortDescription;
+	}
+}
