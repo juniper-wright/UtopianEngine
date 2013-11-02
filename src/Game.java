8c21c9711f69f821fe0aeecc154d2614559aa1f6
phoenixfyre <ianufyrebird@gmail.com>
2013-11-02 03:46:33 -0400

    Add Exception-handling in Game::updateScore()
    Resolves #41.

--- src/Game.java
+++ src/Game.java
@@ -1155,7 +1155,7 @@ public class Game
 		}
 		catch(Exception e)
 		{
-			
+			throw new GameEndException("FATAL ERROR: Unable to parse the UtopiaScore variable from JavaScript as a number. Value: `" + score + "`");
 		}
 	}
 	
